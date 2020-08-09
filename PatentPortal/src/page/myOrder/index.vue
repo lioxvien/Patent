6d06376635f664c8caf218dff8b8b0d90c87866d
liuyanli123 <549809316@qq.com>
2020-08-10 00:13:53 +0800

    我的QQ等，我的订单

---
+++ PatentPortal/src/page/myOrder/index.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="sys-page">
+        <el-tabs v-model="activeName" type="border-card"  @tab-click="toggleTab">
+            <el-tab-pane label="我的出售" name="sale">
+                <SaleBuy :info="activeName" :type="'my'"></SaleBuy>
+            </el-tab-pane>
+            <el-tab-pane label="我的求购" name="buy">
+                <SaleBuy :info="activeName" :type="'my'"></SaleBuy>
+            </el-tab-pane>
+            <el-tab-pane label="我的收藏" name="colloction">
+                我的收藏
+            </el-tab-pane>
+        </el-tabs>
+        <SideButton :page="'home'"></SideButton>
+    </div>
+</template>
+
+<script>
+    import SideButton from '../../components/SideButton'
+    import SaleBuy from '../../components/SaleBuy'
+
+  export default {
+      name: 'myOrder',
+      components: { SideButton,SaleBuy },
+      data() {
+          return {
+              activeName: 'sale',
+          }
+      },
+      methods: {
+          toggleTab(tab) {
+              this.activeName = tab.name;
+          },
+      }
+  }
+</script>
+
+<style scoped>
+    .sys-page >>> .el-tabs--border-card>.el-tabs__header{
+        background: #fca851;
+    }
+    .sys-page >>> .el-tabs--border-card>.el-tabs__header .el-tabs__item {
+        color: #303133;
+    }
+    .sys-page >>> .el-tabs--border-card>.el-tabs__header .el-tabs__item.is-active {
+        font-size: 15px;
+    }
+</style>
